7fb3c16180d0971bce5f69f8e69a93735b7259a4
Vitalii Kozachenko <kozachenko.vitaliy@gmail.com>
2024-07-12 12:23:05 +0200

    Update ht2-2.py

--- ht2-2.py
+++ ht2-2.py
@@ -1,6 +1,6 @@
-import re
+from lxml import html
 
-# Ваш HTML-код як рядок
+
 html_content = '''
 <!DOCTYPE html>
 <html>
@@ -17,17 +17,15 @@ html_content = '''
 </html>
 '''
 
-# Регулярні вирази для пошуку елементів
-search_query_pattern = re.compile(r'<input[^>]*id="searchQuery"[^>]*>')
-region_input_pattern = re.compile(r'<input[^>]*id="regionInput"[^>]*>')
-search_button_pattern = re.compile(r'<button[^>]*id="searchButton"[^>]*>.*?</button>', re.DOTALL)
+# Створення дерева HTML
+tree = html.fromstring(html_content)
 
-# Знаходження елементів
-search_query_input = search_query_pattern.search(html_content)
-region_input = region_input_pattern.search(html_content)
-search_button = search_button_pattern.search(html_content)
+# XPath вирази
+search_query_input = tree.xpath('//input[@id="searchQuery"]')
+region_input = tree.xpath('//input[@id="regionInput"]')
+search_button = tree.xpath('//button[@id="searchButton"]')
 
 # Вивід результатів
-print("Search Query Input:", search_query_input.group() if search_query_input else "Not found")
-print("Region Input:", region_input.group() if region_input else "Not found")
-print("Search Button:", search_button.group() if search_button else "Not found")
+print("Search Query Input:", search_query_input[0].attrib if search_query_input else "Not found")
+print("Region Input:", region_input[0].attrib if region_input else "Not found")
+print("Search Button:", search_button[0].attrib if search_button else "Not found")
